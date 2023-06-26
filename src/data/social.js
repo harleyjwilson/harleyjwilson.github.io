a06fdf2cca3bc5a27f93690d64dbec90311bb229
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2023-06-26 19:48:00 +0800

    Initial landing page

---
+++ src/data/social.js
@@ -0,0 +1,22 @@
+export const SOCIAL = [
+    {
+        path: "mailto:hi@harleyjwilson.com",
+        title: "Email",
+        icon: "/icons/mail.svg",
+    },
+    {
+        path: "https://twitter.com/harleyjwilson",
+        title: "Twitter",
+        icon: "/icons/twitter.svg",
+    },
+    {
+        path: "https://github.com/harleyjwilson",
+        title: "GitHub",
+        icon: "/icons/github.svg",
+    },
+    {
+        path: "https://linkedin.com/in/harleyjwilson/",
+        title: "LinkedIn",
+        icon: "/icons/linkedin.svg",
+    },
+];
