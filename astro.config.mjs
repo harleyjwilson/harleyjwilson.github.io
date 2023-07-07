9c52f2ed7c28d1d2027d39710640363c0619519c
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2023-07-07 11:54:33 +0800

    Implement redesigned index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    site: "https://harleyjwilson.com",
+    integrations: [tailwind(), mdx(), sitemap()],
 });
