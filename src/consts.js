20194e20e7f77fe9e3b27dce172d4306277bd1ae
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2025-04-25 15:11:49 +0800

    Update consts

--- src/consts.js
+++ src/consts.js
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Harley Wilson";
-export const SITE_DESCRIPTION = "Harley Wilson Personal Site";
-export const SITE_URL = "https://harleyjwilson.github.io";
+export const SITE_DESCRIPTION = "Harley Wilson's Personal Site";
+export const SITE_URL = "https://harleyjwilson.com";
 export const SEARCH_URL = "https://duckduckgo.com";
